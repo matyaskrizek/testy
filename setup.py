553ff98dbe05052f4a7ba81c85d540ef407b98b4
mkrizek <matyask99@gmail.com>
2022-12-06 19:15:36 -0800

    second

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='pippychippyprod',
+    name='Matyasproject',
     version='1.0.0',
     author="Matyas aka chip",
     author_email='mkrizek@dons.usfca.edu',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
+    url='https://github.com/matyaskrizek/testy',
     keywords='example project',
     install_requires=[
           'scikit-learn',
